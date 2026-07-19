7e0cb073da2527886627535fce74c418eeec46f7
Talon De La Torre <tddmedia1@gmail.com>
2026-07-18 23:51:13 -0700

    pagespeed fix attemp 1

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,14 +1,14 @@
 module.exports = {
-    name: "Summit Records",
-    email: "talondelatorre7@gmail.com",
+    name: "DEL Records",
+    email: "info@delrecords.com",
     phoneForTel: "909-919-0912",
     phoneFormatted: "(909) 919-0912",
     socials: {
-        facebook: "https://www.facebook.com/log00001",
-        instagram: "https://www.instagram.com/thesummitrecords",
+        facebook: "https://www.facebook.com/delrecords",
+        instagram: "https://www.instagram.com/delrecords",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    domain: "https://www.example.com",
+    domain: "https://www.delrecords.com",
     // Passing the isProduction variable for use in HTML templates
     isProduction: process.env.ELEVENTY_ENV === "PROD",
 };
